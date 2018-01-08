d959582f6476dea16e283396a02c56308d302542
eawarnong <salisa_cct@hotmail.com>
2018-01-08 16:55:05 +0700

    Add state

--- search_bar.js
+++ search_bar.js
@@ -36,3 +36,25 @@ class SearchBar extends Component {
     // return <input onChange={(event) => console.log(event.target.value);}/>;
   }
 }
+
+///// Add State /////
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {term : ''};
+  }
+  
+  render() {
+    // save state every times state change
+    // print out typed value in real time
+    return (
+      <div>
+        <input onChange={event => this.setState({term : event.target.value}) />
+        Value of input : {this.state.term}
+      </div>
+    );
+  }
+}

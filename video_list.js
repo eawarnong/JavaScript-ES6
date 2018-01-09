dc0ed0e848c07e2d6ecdfed542bc351c02ec615b
eawarnong <salisa_cct@hotmail.com>
2018-01-09 17:45:19 +0700

    Video Selection

--- video_list.js
+++ video_list.js
@@ -29,5 +29,25 @@ const VideoList = (props) => {
 
 export default VideoList;
 
+//// Video Selection /////
+import React from 'react';
+import VideoListItem from './video_list_item.js';
+
+const VideoList = (props) => {
+  const videoItems = props.videos.map((video) => {
+    return 
+      <VideoListItem 
+        onVideoSelect={prop.onVideoSelect}
+        key={video.etag} 
+        video={video} />;
+  });
+  
+  return (
+    <ul className="col-md-4 list-group">
+      {videoItems}
+    </ul>
+  );
+}
+
 
 

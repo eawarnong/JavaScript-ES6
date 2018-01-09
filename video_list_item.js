5912ccd82b6fa183f09c19b06ec342634263800d
eawarnong <salisa_cct@hotmail.com>
2018-01-09 17:46:36 +0700

    Video Selection

--- video_list_item.js
+++ video_list_item.js
@@ -29,3 +29,23 @@ const VideoListItem = ({ video }) => {
 }
 
 export default VideoListItem;
+
+///// Video Selection /////
+import React from 'react'
+
+const VideoListItem = ({ video }) => {
+  const imageUrl = video.snippet.thumbnails.default.image;
+  
+  return (
+    <li onClick={() => onVideoSelect(video)} className="list-group-item">
+      <div className="video-list media">
+        <div className="media-left">
+          <img className="media-object" src={imageUrl} />
+        </div>
+        <div className="media-body">
+          <div className="media-heading">{video.snippet.title}</div>
+        </div>
+      </div>
+    </li>
+  );
+}

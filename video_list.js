cde0ef6382ca243f61494650450106833584196a
eawarnong <salisa_cct@hotmail.com>
2018-01-09 12:15:39 +0700

    Add VideoItem

--- video_list.js
+++ video_list.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
 
 const VideoList = (props) => {
   return (
     <ul className="col-md-4 list-group">
       {props.videos.length}
+    </ul>
+  );
+}
+
+export default VideoList;
+
+///// Add VideoItem /////
+import React from 'react';
+import VideoListItem from './video_list_item.js';
+
+const VideoList = (props) => {
+  const videoItems = props.videos.map((video) => {
+    return <VideoListItem video={video} />;
+  });
+  
+  return (
+    <ul className="col-md-4 list-group">
+      {videoItems}
     </ul>
   );
 }

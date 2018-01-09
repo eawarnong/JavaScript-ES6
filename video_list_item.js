e2fc68583dcdcc5e14ad6198a56894d19361a4e7
eawarnong <salisa_cct@hotmail.com>
2018-01-09 14:14:57 +0700

    Video List Items

--- video_list_item.js
+++ video_list_item.js
@@ -5,3 +5,27 @@ const VideoListItem = (props) => {
 }
 
 export default VideoListItem;
+
+///// Video List Items /////
+import React from 'react'
+
+// the video argument is just like 
+// const video = props.video
+const VideoListItem = ({ video }) => {
+  const imageUrl = video.snippet.thumbnails.default.image;
+  
+  return (
+    <li className="list-group-item">
+      <div className="video-list media">
+        <div className="media-left">
+          <img className="media-object src={imageUrl}/>
+        </div>
+        <div className="media-body">
+          <div className="media-heading">{video.snippet.title}</div>
+        </div>
+      </div>
+    </li>
+  );
+}
+
+export default VideoListItem;
